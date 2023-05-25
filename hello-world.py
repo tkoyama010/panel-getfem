5d9a536acb40d06b70cc1ad2874d71b4d1e0330a
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-25 17:50:08 +0900

    Add tabs

--- hello-world.py
+++ hello-world.py
@@ -3,9 +3,11 @@ import panel as pn
 pn.extension()
 
 tabs = pn.Tabs(
-    ("red", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
-    ("blue", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
-    ("green", pn.Spacer(styles=dict(background="green"), width=500, height=1000)),
+    ("Mesher", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
+    ("Mesh", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
+    ("Fem", pn.Spacer(styles=dict(background="green"), width=500, height=1000)),
+    ("Integ", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
+    ("Model", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
 )
 
 pn.Row(
