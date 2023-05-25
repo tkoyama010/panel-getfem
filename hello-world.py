0c464aa829deea3c7f2d790593d68744154c83f8
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-25 18:48:14 +0900

    Fix layout

--- hello-world.py
+++ hello-world.py
@@ -3,11 +3,10 @@ import panel as pn
 pn.extension()
 
 tabs = pn.Tabs(
-    ("Mesher", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
     ("Mesh", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
+    ("Model", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
     ("Fem", pn.Spacer(styles=dict(background="green"), width=500, height=1000)),
     ("Integ", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
-    ("Model", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
 )
 
 pn.Row(
