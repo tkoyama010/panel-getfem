fbff8607e8446eb294b978d89454fb8935351c8d
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-25 17:37:44 +0900

    Add Tab layout

--- hello-world.py
+++ hello-world.py
@@ -2,13 +2,16 @@ import panel as pn
 
 pn.extension()
 
-from bokeh.plotting import figure
+tabs = pn.Tabs(
+    ("red", pn.Spacer(styles=dict(background="red"), width=100, height=100)),
+    ("blue", pn.Spacer(styles=dict(background="blue"), width=100, height=100)),
+    ("green", pn.Spacer(styles=dict(background="green"), width=100, height=100)),
+    closable=True,
+)
 
-p1 = figure(width=300, height=300, name='Scatter')
-p1.scatter([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0])
-
-p2 = figure(width=300, height=300, name='Line')
-p2.line([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0])
-
-tabs = pn.Tabs(('Scatter', p1), p2)
-tabs.show()
+pn.Row(
+    tabs,
+    tabs.clone(active=1, tabs_location="right"),
+    tabs.clone(active=2, tabs_location="below"),
+    tabs.clone(tabs_location="left"),
+).show()
