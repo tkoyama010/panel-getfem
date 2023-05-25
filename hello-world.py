0bd230746f5e568b5dd99deef0c340accc8ba552
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-25 17:45:07 +0900

    Fix layout

--- hello-world.py
+++ hello-world.py
@@ -3,15 +3,11 @@ import panel as pn
 pn.extension()
 
 tabs = pn.Tabs(
-    ("red", pn.Spacer(styles=dict(background="red"), width=100, height=100)),
-    ("blue", pn.Spacer(styles=dict(background="blue"), width=100, height=100)),
-    ("green", pn.Spacer(styles=dict(background="green"), width=100, height=100)),
-    closable=True,
+    ("red", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
+    ("blue", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
+    ("green", pn.Spacer(styles=dict(background="green"), width=500, height=1000)),
 )
 
 pn.Row(
     tabs,
-    tabs.clone(active=1, tabs_location="right"),
-    tabs.clone(active=2, tabs_location="below"),
-    tabs.clone(tabs_location="left"),
 ).show()
