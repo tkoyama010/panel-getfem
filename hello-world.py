1c390aa4eff550de1428ed117b2ce8abb04defaa
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-26 09:27:46 +0900

    Add mesh

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,12 @@
+import getfem as gf
 import panel as pn
+import pyvista as pv
+from IPython.display import IFrame
 
+pv.set_plot_theme("document")
 pn.extension()
+
+m = gf.Mesh("import", "gid", "tripod.GiD.msh")
 
 tabs = pn.Tabs(
     ("Mesh", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
