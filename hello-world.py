f8c840c0caaf993e404854f103a16533096e1e4b
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-27 10:43:24 +0900

    Remove Fem and Integ

--- hello-world.py
+++ hello-world.py
@@ -11,8 +11,6 @@ m = gf.Mesh("import", "gid", "tripod.GiD.msh")
 tabs = pn.Tabs(
     ("Mesh", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
     ("Model", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
-    ("Fem", pn.Spacer(styles=dict(background="green"), width=500, height=1000)),
-    ("Integ", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
 )
 
 pn.Row(
