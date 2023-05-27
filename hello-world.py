31c9f17c6ae71be00e65b2fe8282690bd61c2c28
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-27 15:45:27 +0900

    Add main

--- hello-world.py
+++ hello-world.py
@@ -9,6 +9,20 @@ pn.extension()
 m = gf.Mesh("import", "gid", "tripod.GiD.msh")
 m.export_to_vtk("tripod.vtk", "ascii")
 
+mesh = pv.read("tripod.vtk")
+plotter = pv.Plotter(notebook=True)
+plotter.add_mesh(mesh)
+
+def handler(viewer, src, **kwargs):
+    return IFrame(src, "100%", "1000px")
+
+
+iframe = plotter.show(
+    jupyter_backend="trame",
+    jupyter_kwargs=dict(handler=handler),
+    return_viewer=True,
+)
+
 tabs = pn.Tabs(
     ("Mesh", pn.Spacer(styles=dict(background="red"), width=500, height=1000)),
     ("Model", pn.Spacer(styles=dict(background="blue"), width=500, height=1000)),
@@ -17,6 +31,7 @@ tabs = pn.Tabs(
 template = pn.template.MaterialTemplate(
     title='GetFEM',
     sidebar=[tabs],
+    main=[pn.panel(iframe, width=1500, height=250)],
 )
 
 template.servable()
